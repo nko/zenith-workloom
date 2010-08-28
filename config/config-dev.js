0f984e3c61da2cffe86ac629eff1dc9405594068
Clint Andrew Hall <clint@clintandrewhall.com>
2010-08-27 21:25:02 -0500

    gogogo

---
+++ config/config-dev.js
@@ -0,0 +1,21 @@
+
+var config = {
+    database : {
+        host : process.env['NODE_MONGO_DRIVER_HOST'],
+        port : process.env['NODE_MONGO_DRIVER_PORT'],
+        name : process.env['NODE_MONGO_DATABASE_NAME']
+    },
+	facebook : {
+		id : process.env['NODE_FACEBOOK_ID'],
+		secret : process.env['NODE_FACEBOOK_SECRET'],
+		callbackUrl : process.env['NODE_FACEBOOK_CALLBACK']
+	},
+	twitter : {
+		key : process.env['NODE_TWITTER_KEY'],
+		secret : process.env['NODE_TWITTER_SECRET'],
+		requestUrl : "https://api.twitter.com/oauth/request_token",
+		responseUrl : "https://api.twitter.com/oauth/access_token"
+	}
+};
+
+exports.config = config;
